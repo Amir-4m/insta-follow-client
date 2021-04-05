524779168898ac27e4ef9203c99e27fdcf4d3c1c
Arash F <arash@hami.blue>
2021-04-05 16:46:32 +0430

    PREV

--- apps/insta_users/models.py
+++ apps/insta_users/models.py
@@ -3,24 +3,29 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class InstaUser(models.Model):
-    STATUS_ACTIVE = "active"
-    STATUS_BLOCKED = "blocked"
-    STATUS_TEMPORARY_BLOCKED = "temporary blocked"
-    STATUS_REMOVED = "removed"
+    STATUS_ACTIVE = 10
+    STATUS_BLOCKED_TEMP = 20
+    STATUS_BLOCKED = 21
+    STATUS_REMOVED = 30
 
-    STATUS = ((STATUS_ACTIVE, _("active")),
-              (STATUS_BLOCKED, _("blocked")),
-              (STATUS_TEMPORARY_BLOCKED, _("temporary blocked")),
-              (STATUS_REMOVED, _("removed")))
+    STATUS_CHOICES = (
+        (STATUS_ACTIVE, _("active")),
+        (STATUS_BLOCKED, _("blocked")),
+        (STATUS_BLOCKED_TEMP, _("blocked temporary")),
+        (STATUS_REMOVED, _("removed"))
+    )
 
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
-    username = models.CharField(_("username"), max_length=128)
+
+    username = models.CharField(_("username"), max_length=64)
     password = models.CharField(_("password"), max_length=128)
-    user_id = models.BigIntegerField(_("user ID"), blank=True, null=True)
+    user_id = models.PositiveBigIntegerField(_("user ID"), unique=True, blank=True, null=True)
     session = models.TextField(_("session"), blank=True)
+
     server_key = models.UUIDField(_('server Key'), blank=True, null=True)
-    status = models.CharField(_("Status"), choices=STATUS, default=STATUS_ACTIVE, max_length=100)
+
+    status = models.PositiveSmallIntegerField(_("Status"), choices=STATUS_CHOICES, default=STATUS_ACTIVE, db_index=True)
 
     def __str__(self):
         return self.username
