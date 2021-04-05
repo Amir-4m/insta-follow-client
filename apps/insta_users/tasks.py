065dd2f02487071f249f19bee91076adde22db3d
Arash F <arash@hami.blue>
2021-04-05 16:41:00 +0430

    Code Cleanup

--- apps/insta_users/tasks.py
+++ apps/insta_users/tasks.py
@@ -1,18 +1,23 @@
 import logging
-from datetime import datetime
-
 import requests
+
+from datetime import datetime
+from django.conf import settings
 from celery import shared_task
 
 from .models import InstaUser
 
 logger = logging.getLogger(__name__)
 
-BASE_URL = "https://www.instagram.com/"
-LOGIN_URL = BASE_URL + "accounts/login/ajax/"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; ) Gecko/20100101 Firefox/65.0"
 
-headers = {
+INSTA_FOLLOW_LOGIN_URL = f'{settings.INSTAFOLLOW_BASE_URL}/api/v1/instagram/login-verification/'
+
+INSTAGRAM_BASE_URL = 'https://www.instagram.com'
+INSTAGRAM_LOGIN_URL = f'{INSTAGRAM_BASE_URL}/accounts/login/ajax/'
+
+# TODO: for later use
+"""
+INSTAGRAM_HEADERS = {
     "Host": "www.instagram.com",
     "Accept": "*/*",
     "Accept-Language": "en-US,en;q=0.5",
@@ -23,25 +28,72 @@ headers = {
     "DNT": "1",
     "Connection": "keep-alive",
 }
-session = requests.Session()
-session.headers = {'user-agent': USER_AGENT, 'Referer': BASE_URL}
+"""
 
 
 @shared_task
-def instagram_login(pk):
+def instagram_login(insta_user_id):
     try:
-        usr = InstaUser.objects.get(id=pk)
-        str_time = datetime.now().timestamp()
-        pswd = f'#PWD_INSTAGRAM_BROWSER:0:{str_time}:{usr.password}'
-        resp = session.get(BASE_URL)
-        session.headers.update({'X-CSRFToken': resp.cookies['csrftoken']})
-        login_data = {'username': usr.username, 'enc_password': pswd}
-        login_resp = session.post(LOGIN_URL, data=login_data, allow_redirects=True)
+        insta_user = InstaUser.objects.get(id=insta_user_id, session='')
+    except InstaUser.DoesNotExist as e:
+        logger.warning(f'[getting instafollow uuid failed]-[instauser id: {insta_user_id}]-[exc: {e}]')
+        return
+
+    session = requests.Session()
+    session.headers = {
+        'Referer': INSTAGRAM_BASE_URL,
+        'user-agent': "Mozilla/5.0 (Windows NT 10.0; ) Gecko/20100101 Firefox/65.0",
+    }
+
+    resp = session.get(INSTAGRAM_BASE_URL)
+    session.headers.update({'X-CSRFToken': resp.cookies['csrftoken']})
+    login_data = {'username': insta_user.username, 'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{datetime.now().timestamp()}:{insta_user.password}'}
+
+    try:
+        login_resp = session.post(INSTAGRAM_LOGIN_URL, data=login_data, allow_redirects=True)
         session.headers.update({'X-CSRFToken': resp.cookies['csrftoken']})
         if login_resp.json()['authenticated']:
-            usr.session = str(session.cookies['sessionid'])
-            usr.user_id = session.cookies['ds_user_id']
-            usr.save()
-            logger.info(f"log in succeeded with the credintial of [username: {usr.username} -- password: {usr.password}]")
+            insta_user.session = str(session.cookies)
+            insta_user.user_id = session.cookies['ds_user_id']
+            insta_user.save()
+            logger.info(f"log in succeeded for {insta_user.username}")
     except Exception as e:
-        logger.error(f"log in failed with the credintial of [username: {usr.username} -- password: {usr.password}]")
+        logger.error(f"log in failed for {insta_user.username} - [error: {e}]")
+
+
+@shared_task
+def insta_follow_login(insta_user_id):
+    # getting InstaUser object
+    try:
+        insta_user = InstaUser.objects.get(id=insta_user_id)
+    except InstaUser.DoesNotExist as e:
+        logger.warning(f'[getting instafollow uuid failed]-[instauser id: {insta_user_id}]-[exc: {e}]')
+        return
+
+    # getting uuid from instafollow api
+    parameter = dict(
+        instagram_user_id=insta_user.user_id,
+        instagram_username=insta_user.username,
+        session_id=insta_user.session
+    )
+    url = INSTA_FOLLOW_LOGIN_URL
+    logger.debug(f"[calling api]-[URL: {url}]-[data: {parameter}]")
+
+    try:
+        response = requests.post(url, json=parameter)
+        insta_user.server_key = response.json().get('uuid')
+        insta_user.save()
+    except requests.exceptions.HTTPError as e:
+        logger.warning(
+            f'[making request failed]-[URL: {url}]-[status code: {e.response.status_code}]'
+            f'-[response err: {e.response.text}]-[exc: {e}]'
+        )
+        return
+    except requests.exceptions.ConnectTimeout as e:
+        logger.critical(f'[request failed]-[URL: {url}]-[exc: {e}]')
+        return
+    except Exception as e:
+        logger.error(f'[request failed]-[URL: {url}]-[exc: {e}]')
+        return
+
+    return insta_user.server_key
--- apps/insta_users/utils.py
+++ apps/insta_users/utils.py
@@ -1,20 +1,14 @@
 import logging
-import requests
 
 from datetime import datetime
 from base64 import b64decode, b64encode
 
-from .models import InstaUser
-from django.conf import settings
 from django.utils.encoding import smart_text
 
 from Crypto.Cipher import AES
 from Crypto import Random
 
 logger = logging.getLogger(__file__)
-
-
-LOGIN_VERIFICATION_URL = f'{settings.INSTAFOLLOW_BASE_URL}/api/v1/instagram/login-verification/'
 
 
 class CryptoService:
@@ -58,40 +52,4 @@ def get_insta_follow_token(insta_user_server_key):
     token = CryptoService(dt.strftime("%d%m%y%H") + dt.strftime("%d%m%y%H")).encrypt(str(insta_user_server_key))
     return f'Token {smart_text(token)}'
 
-
-def insta_follow_auth(insta_user_id):
-    # getting InstaUser object
-    try:
-        insta_user = InstaUser.objects.get(id=insta_user_id)
-    except InstaUser.DoesNotExist as e:
-        logger.warning(f'[getting instafollow uuid failed]-[instauser id: {insta_user_id}]-[exc: {e}]')
-        return
-
-    # getting uuid from instafollow api
-    parameter = dict(
-        instagram_user_id=insta_user.user_id,
-        instagram_username=insta_user.username,
-        session_id=insta_user.session
-    )
-    url = LOGIN_VERIFICATION_URL
-    logger.debug(f"[calling api]-[URL: {url}]-[data: {parameter}]")
-
-    try:
-        response = requests.post(url, json=parameter)
-        insta_user.server_key = response.json().get('uuid')
-        insta_user.save()
-    except requests.exceptions.HTTPError as e:
-        logger.warning(
-            f'[making request failed]-[URL: {url}]-[status code: {e.response.status_code}]'
-            f'-[response err: {e.response.text}]-[exc: {e}]'
-        )
-        return
-    except requests.exceptions.ConnectTimeout as e:
-        logger.critical(f'[request failed]-[URL: {url}]-[exc: {e}]')
-        return
-    except Exception as e:
-        logger.error(f'[request failed]-[URL: {url}]-[exc: {e}]')
-        return
-
-    return insta_user.server_key
 
