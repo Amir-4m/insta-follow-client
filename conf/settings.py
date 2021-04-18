95b2f7d31079a204d2a5ba11d796d997555bf744
Amir Amiri <amir.amiri@zamann-pharma.com>
2021-04-18 14:34:34 +0430

    cookies app implemented

--- conf/settings.py
+++ conf/settings.py
@@ -33,6 +33,7 @@ SECRET_KEY = config("SECRET_KEY")
 # Application definition
 INSTALLED_APPS = [
     # Local
+    'apps.cookies',
     'apps.insta_users',
     'apps.proxies',
     'apps.simulators',
