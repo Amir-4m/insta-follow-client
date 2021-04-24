2ace6d47515a01ca1340a533ac9258a085f55c10
Arash F <arash@hami.blue>
2021-04-24 16:53:06 +0430

    rearranged for docker CI

--- apps/insta_users/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InstaUsersConfig(AppConfig):
-    name = 'apps.insta_users'
--- utils/log.py
+++
@@ -1,7 +0,0 @@
-import logging
-from django.conf import settings
-
-
-class RequireDevelTrue(logging.Filter):
-    def filter(self, record):
-        return settings.DEVEL
