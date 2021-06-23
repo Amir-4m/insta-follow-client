eb11f0bb519f013daedc00d7b2812512f8f0ac2a
amir <a.amiri@hami.blue>
2021-06-23 18:38:53 +0430

    tasks updated

--- project/apps/simulators/tasks.py
+++ project/apps/simulators/tasks.py
@@ -204,9 +204,9 @@ def follow_active_users(insta_user_id):
         time.sleep(settings.INSTA_FOLLOW_SETTINGS[f"delay_{action}"])
 
 
-@periodic_task(run_every=crontab(minute='*/1'))
+@periodic_task(run_every=crontab(minute='*/30'))
 def random_new_task():
-    new_insta_user_ids = InstaUser.objects.new().values_list('user_id', flat=True).order_by('?')[:1]
+    new_insta_user_ids = InstaUser.objects.new().values_list('user_id', flat=True).order_by('?')[:5]
     for insta_user_id in new_insta_user_ids:
         action_to_call = globals()[random.choice(
             random.choices((
@@ -220,9 +220,9 @@ def random_new_task():
         action_to_call.apply_async(args=(insta_user_id,), countdown=3)
 
 
-@periodic_task(run_every=crontab(minute='*/1'))
+@periodic_task(run_every=crontab(minute='*/10'))
 def random_manage_task():
-    manageable_insta_user_ids = InstaUser.objects.manageable().values_list('user_id', flat=True).order_by('?')[:1]
+    manageable_insta_user_ids = InstaUser.objects.manageable().values_list('user_id', flat=True).order_by('?')[:5]
     for insta_user_id in manageable_insta_user_ids:
         action_to_call = globals()[random.choice(
             random.choices((
